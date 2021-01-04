016a95670d7a30951bbc56e09c42322a5522d290
Ahrorbek <Ahror.n.b@gmail.com>
2021-01-04 16:09:46 +0500

    Login bg changed

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import {useRouter} from "next/router";
 import {useState} from 'react';
 import {DOMAIN, queryData} from "../services/requestService";
+import Image from "next/image";
 
 export default function Home() {
 
@@ -19,9 +20,9 @@ export default function Home() {
         await axios.post(DOMAIN + '/api/auth/login', payload).then(async (res) => {
                 if (res.data.statusCodeValue === 200) {
                     localStorage.setItem("EducationCenterToken", res.data.body.accessToken);
-                    await queryData({path: "/api/menu", method: 'get'}).then(res => {
+                    queryData({path: "/api/menu", method: 'get'}).then(res => {
                         localStorage.setItem("menu", res.data.object.length)
-                        if(res.data.object.length===3) {
+                        if (res.data.object.length > 1) {
                             router.push("/students")
                         } else {
                             router.push("/groups")
@@ -37,39 +38,50 @@ export default function Home() {
 
     if (loading) return (<div className='position-relative vh-100 vw-100 text-center'>
         <div
-            className='position-absolute bg-light w-75 rounded px-0 py-2 p-md-5'
+            className='position-absolute w-75 rounded px-0 py-2 p-md-5'
             style={{zIndex: '3', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
-            Oka biroz kutib turasiz endi, shundo serverga borib kevomman . . .
+            <Image
+                src="/button-loading.gif"
+                alt="loading..."
+                width={45}
+                height={45}/>
+                <br/>
+                <br/>
+                Loading . . .
+            {/*Oka biroz kutib turasiz endi, shundo serverga borib kevomman . . .*/}
         </div>
     </div>)
     else return (
-        <>
+        <div style={{backgroundImage: `url('./login-bg.jpg')`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh', overflow: 'hidden'}}>
             {/*<Layout home>*/}
+            <br/>
+            <br/>
+            <br/>
             <br/>
             <div className="row mt-5">
                 <div className="col-10 offset-1 col-md-4 offset-md-4">
 
-                    <div className='card text-center'>
-                        <div className='card-header text-success font-weight-bold'>Log In</div>
+                    <div className='card bg-transparent text-center'>
+                        <h3 className='card-header text-monospace text-info'>Log In</h3>
                         <div className="card-body p-2 p-md-3">
-                            <input className='form-control form-group mt-2 text-center'
+                            <input className='form-control form-group mt-2 text-center text-info'
                                    onChange={(event) => changeHandler(event)}
                                    value={payload.phoneNumber} type="text"
                                    placeholder='Login'
                                    name="phoneNumber"/>
-                            <input className='form-control form-group text-center'
+                            <input className='form-control form-group text-center text-danger'
                                    onChange={(event) => changeHandler(event)}
                                    value={payload.password}
                                    placeholder='Password'
                                    type="password"
                                    name="password"/>
 
-                            <button onClick={login} className='btn btn-primary form-group text-monospace'>Log In
+                            <button onClick={login} className='btn btn-primary bg-transparent form-group text-monospace'>Log In
                             </button>
                         </div>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
--- pages/students.js
+++ pages/students.js
@@ -37,6 +37,7 @@ export default function Students() {
             }
             setLoading(false)
         })
+        console.log(groups);
     }
 
     const changePage = () =>{
